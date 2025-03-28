e23eb8b947e8ebe5039b4d68cb59a2afd9b9fb1c
Luis Torres <lmtorresv@eafit.edu.co>
2025-03-28 14:13:16 -0500

    [server] Print date and time in logs

--- battlesocket-server/src/logger.c
+++ battlesocket-server/src/logger.c
@@ -1,6 +1,11 @@
 #include "logger.h"
+
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
+#define DATE_FORMAT "%F %T"
+#define BUFFER_SIZE 1024
 
 FILE *log_file = NULL;
 
@@ -18,11 +23,29 @@ void
 log_event (const char *event)
 {
   time_t now = time (NULL);
-  fprintf (stdout, "[%ld] %s\n", now, event);
+  if (now == (time_t)(-1))
+    {
+      fprintf (stderr, "Failed to get current time.\n");
+      exit (EXIT_FAILURE);
+    }
+
+  struct tm datetime;
+  if (localtime_r (&now, &datetime) == NULL)
+    {
+      fprintf (stderr, "Failed to get local time.\n");
+      exit (EXIT_FAILURE);
+    }
+
+  char date_as_text[BUFFER_SIZE] = { 0 };
+  strftime (date_as_text, BUFFER_SIZE, DATE_FORMAT, &datetime);
+
+  char log_message[BUFFER_SIZE * 2] = { 0 };
+  snprintf (log_message, BUFFER_SIZE * 2, "%s %s\n", date_as_text, event);
+
+  fputs (log_message, stdout);
   if (log_file != NULL)
     {
-      fprintf (log_file, "[%ld] %s\n", now, event);
-      fflush (log_file);
+      fputs (log_message, log_file);
     }
 }
 
