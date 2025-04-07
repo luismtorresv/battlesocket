luismtorresv/battlesocket

commit 735e57a3fd4618fae76b7ba04804f7a5099865a8

[server] Handle disconnection case of `recv`
From §5.7 of Beej’s Guide to Network Programming: Using Internet Sockets
by Brian “Beej Jorgensen” Hall (v 3.2.7):

> Wait! recv() can return 0. This can mean only one thing: the remote
> side has closed the connection on you! A return value of 0 is recv()’s
> way of letting you know this has occurred.

battlesocket-server/src/server.c
@@ -281,11 +281,17 @@ play_game (int server_fd)
       memset (recv_buffer, 0, sizeof (recv_buffer));
       int bytes_read = recv (get_current_socket_fd (single_room), recv_buffer,
                              sizeof (recv_buffer) - 1, 0);
-      if (bytes_read <= 0)
+      if (bytes_read == 0)
         {
-          log_event ("Failed receiving data or client disconnection");
+          log_event ("[ERROR] Client disconnection.");
           break;
         }
+      else if (bytes_read == 1)
+        {
+          log_event ("[ERROR] Failed to recv data.");
+          break;
+        }
+
       int newline_pos = strcspn (recv_buffer, "\r\n");
       recv_buffer[newline_pos] = '\0';
 

commit 90cbe707754522997f3c5a4c39ff768ab0f14122

[server] Fix comparison of `recv()`'s return value
Error introduced in 735e57a3fd4618fae76b7ba04804f7a5099865a8.

battlesocket-server/src/server.c
@@ -269,7 +269,7 @@ handle_client (void *arg)
           log_event (LOG_INFO, "Client disconnection.");
           break;
         }
-      else if (bytes_read == 1)
+      else if (bytes_read == -1)
         {
           log_event (LOG_ERROR, "Failed to recv data.");
           break;