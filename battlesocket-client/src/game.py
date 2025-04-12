luismtorresv/battlesocket

commit 591f1cb6836b20e710f565591e7a018d62d0026d

[client] client can now surrender/give up.

battlesocket-client/src/game.py
@@ -1,5 +1,5 @@
 import constants
-from protocol import Send_Protocol
+from protocol import Send
 from re import match
 
 
@@ -37,6 +37,7 @@ class Game:
         self.current_player = None
         self.player_letter = None
         self.turn_time = None
+        self.has_surrendered = False
 
     def place_ships(self, board):
         battleships_str = board.split(";")  # Stores all the battleships
@@ -57,7 +58,7 @@ class Game:
             # For cruiser ships, a lowercase 'c' is placed.
             if "cruiser" not in ship_name:
                 ship_letter = ship_letter.upper()
-            
+
             self.player_board.place_ship(ship_coordinates, ship_letter)
 
     def print_boards(self):
@@ -68,7 +69,11 @@ class Game:
 
     def was_hit(self, message, player):
         if "SUNK" in message:
-            msg_type, coordinate, _, = message.split(" ")
+            (
+                msg_type,
+                coordinate,
+                _,
+            ) = message.split(" ")
             self.remaining_ships -= 1
 
             print(f"A ship sunk! There are {self.remaining_ships} ships remainings.")
@@ -102,29 +107,40 @@ class Game:
             # Substract 1 from each coordinate so it complies with python logic.
             new_set.append((letter - 1, int(number) - 1))
         return new_set
-    
-    def set_current_turn(self,message):
-        _, player_letter, time = message.split(" ") #TODO: timeout. The turn sends the time.
+
+    def set_current_turn(self, message):
+        _, player_letter, time = message.split(
+            " "
+        )  # TODO: timeout. The turn sends the time.
         self.current_player = player_letter
         self.turn_time = time
 
-    def build_shoot_msg(self, client):
+    def fire_shot(self, client):
         print(f"It's player {client.game.current_player}'s turn.")
         if client.game.player_letter == client.game.current_player:
-            coordinate = input("\n").upper().strip()
+            try:
+                coordinate = input("\n").upper().strip()
+            except:
+                KeyboardInterrupt
+                self.input_surrender()
+                return
             expected_input = constants.EXPECTED_INPUT
 
             # Matches the input with the expected input which should take the form: {A-J}{1-10}
             matched = match(expected_input, coordinate)
             if not matched:
-                print("Invalid input, type a coordinate from A-J and a number from 1-10")
-                Send_Protocol.send_input_err_msg(client)
+
+                print(
+                    "Invalid input, type a coordinate from A-J and a number from 1-10"
+                )
+                Send.send_input_err_msg(client)
                 return
-                
-            Send_Protocol.send_shoot_msg(client,coordinate)
 
+            Send.send_shoot_msg(client, coordinate)
 
-
+    def input_surrender(self):
+        self.has_ended = True
+        self.has_surrendered = True
 
     def start_game(self, message):
         # Uses the word 'board' to split the control information into 2 sides.
@@ -138,10 +154,6 @@ class Game:
         if " " not in message:
             print("The game ended.")
             return
-        
-        _,player_letter = message.split(" ")
-        print(f'Game Over. The winner is: Player {player_letter}')
 
-
-        _, winner = message.split(" ")
-        print(f"The winner is: Player: {winner}")
+        _, player_letter = message.split(" ")
+        print(f"Game Over. The winner is: Player {player_letter}")

battlesocket-client/src/client.py
@@ -84,7 +84,8 @@ class Client:
                 pass
             case ProtocolMessages.MSG_TURN:
                 self.game.set_current_turn(message)
-                self.game.build_shoot_msg(self)
+                self.game.fire_shot(self)
+
 
     def handle_bad_request(self, message):
         # TODO: Log this.

battlesocket-client/src/protocol.py
@@ -19,7 +19,12 @@ class Protocol:
     @classmethod
     def build_input_err_msg(cls):
         return "BAD_REQUEST INPUT_ERROR"
-class Send_Protocol:
+    
+    """ @classmethod
+    def build_surrender_msg(cls):
+        return f"END_GAME SURRENDER" """
+
+class Send:
     @classmethod
     def send_shoot_msg(cls,client,message):
         protocol_message = Protocol.build_shoot_msg(message)
@@ -29,3 +34,7 @@ class Send_Protocol:
     def send_input_err_msg(cls,client):
         protocol_message = Protocol.build_input_err_msg()
         client.sockfd.send(protocol_message.encode("ascii"))
+
+    """ def send_surrender_msg(cls,client):
+        protocol_message = Protocol.build_surrender_msg()
+        client.sockfd.send(protocol_message.encode("ascii")) """

commit e3f232020c6525a9302cd6650168139fc13797d7

[client] Fix `KeyboardInterrupt` exception handling
Introduced in 591f1cb6836b.

battlesocket-client/src/game.py
@@ -120,8 +120,7 @@ class Game:
         if client.game.player_letter == client.game.current_player:
             try:
                 coordinate = input("\n").upper().strip()
-            except:
-                KeyboardInterrupt
+            except KeyboardInterrupt:
                 self.input_surrender()
                 return
             expected_input = constants.EXPECTED_INPUT