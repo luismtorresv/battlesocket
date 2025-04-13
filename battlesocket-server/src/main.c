a599b1c191292407b60fac568822c5dae5aa5ea6
Luis Torres <lmtorresv@eafit.edu.co>
2025-04-13 13:10:35 -0500

    [server] Disable `SIGPIPE` signal to avoid dealing with it
    https://forums.freebsd.org/threads/writing-on-a-closed-socket-doesnt-fail.78651/#post-492253

--- battlesocket-server/src/main.c
+++ battlesocket-server/src/main.c
@@ -1,10 +1,14 @@
 #include "server.h"
+#include <signal.h>
 
 int
 main (int argc, char **argv)
 {
   // Set seed using current time. Once for all the program.
   srand (time (NULL));
+
+  // We don't want to deal with "broken pipe" errors.
+  signal (SIGPIPE, SIG_IGN);
 
   char *log_filename = "server.log"; // Default log filename.
   if (argc == 2)
